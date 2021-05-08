c359f98d8b37f3658cf0cfc3005720e49f285df8
Luis Rovirosa Mairlot <luis@codium.team>
2021-05-08 12:55:16 +0200

    [refactor] reduce duplicity

--- src/NumberToLcd.php
+++ src/NumberToLcd.php
@@ -4,16 +4,16 @@ namespace Kata;
 
 class NumberToLcd
 {
-    private const NUMBER_0 = [" _ ","| |","|_|"];
-    private const NUMBER_1 = ["   ","  |","  |"];
-    private const NUMBER_2 = [" _ "," _|","|_ "];
-    private const NUMBER_3 = [" _ "," _|"," _|"];
-    private const NUMBER_4 = ["   ","|_|","  |"];
-    private const NUMBER_5 = [" _ ","|_ "," _|"];
-    private const NUMBER_6 = [" _ ","|_ ","|_|"];
-    private const NUMBER_7 = [" _ ","  |","  |"];
-    private const NUMBER_8 = [" _ ","|_|","|_|"];
-    private const NUMBER_9 = [" _ ","|_|"," _|"];
+    private const NUMBER_0 = [" _ ", "| |", "|_|"];
+    private const NUMBER_1 = ["   ", "  |", "  |"];
+    private const NUMBER_2 = [" _ ", " _|", "|_ "];
+    private const NUMBER_3 = [" _ ", " _|", " _|"];
+    private const NUMBER_4 = ["   ", "|_|", "  |"];
+    private const NUMBER_5 = [" _ ", "|_ ", " _|"];
+    private const NUMBER_6 = [" _ ", "|_ ", "|_|"];
+    private const NUMBER_7 = [" _ ", "  |", "  |"];
+    private const NUMBER_8 = [" _ ", "|_|", "|_|"];
+    private const NUMBER_9 = [" _ ", "|_|", " _|"];
     private const NUMBERS = [
         self::NUMBER_0,
         self::NUMBER_1,
@@ -30,14 +30,15 @@ class NumberToLcd
     public function convert(int $decimal): string
     {
         if ($decimal === 10) {
-            $ten = [
+            $number = [
                 self::NUMBERS[1][0] . self::NUMBERS[0][0],
                 self::NUMBERS[1][1] . self::NUMBERS[0][1],
-                self::NUMBERS[1][2] . self::NUMBERS[0][2]
-                ];
-
-            return implode("\n", $ten) . "\n";
+                self::NUMBERS[1][2] . self::NUMBERS[0][2],
+            ];
+        } else {
+            $number = self::NUMBERS[$decimal];
         }
-        return implode("\n", self::NUMBERS[$decimal]) . "\n";
+
+        return implode("\n", $number) . "\n";
     }
 }
