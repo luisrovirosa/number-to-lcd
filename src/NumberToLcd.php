2a3e42fc78889e69e226d49813d93ec51c817729
Luis Rovirosa Mairlot <luis@codium.team>
2021-05-08 12:39:44 +0200

    [refactor] extract constants

--- src/NumberToLcd.php
+++ src/NumberToLcd.php
@@ -4,36 +4,47 @@ namespace Kata;
 
 class NumberToLcd
 {
+    private const NUMBER_0 = " _ \n| |\n|_|\n";
+    private const NUMBER_1 = "   \n  |\n  |\n";
+    private const NUMBER_2 = " _ \n _|\n|_ \n";
+    private const NUMBER_3 = " _ \n _|\n _|\n";
+    private const NUMBER_4 = "   \n|_|\n  |\n";
+    private const NUMBER_5 = " _ \n|_ \n _|\n";
+    private const NUMBER_6 = " _ \n|_ \n|_|\n";
+    private const NUMBER_7 = " _ \n  |\n  |\n";
+    private const NUMBER_8 = " _ \n|_|\n|_|\n";
+    private const NUMBER_9 = " _ \n|_|\n _|\n";
+
     public function convert(int $decimal): string
     {
         if ($decimal === 2) {
-            return " _ \n _|\n|_ \n";
+            return self::NUMBER_2;
         }
         if ($decimal === 3) {
-            return " _ \n _|\n _|\n";
+            return self::NUMBER_3;
         }
         if ($decimal === 4) {
-            return "   \n|_|\n  |\n";
+            return self::NUMBER_4;
         }
         if ($decimal === 5) {
-            return " _ \n|_ \n _|\n";
+            return self::NUMBER_5;
         }
         if ($decimal === 6) {
-            return " _ \n|_ \n|_|\n";
+            return self::NUMBER_6;
         }
         if ($decimal === 7) {
-            return " _ \n  |\n  |\n";
+            return self::NUMBER_7;
         }
         if ($decimal === 8) {
-            return " _ \n|_|\n|_|\n";
+            return self::NUMBER_8;
         }
         if ($decimal === 9) {
-            return " _ \n|_|\n _|\n";
+            return self::NUMBER_9;
         }
         if ($decimal === 0) {
-            return " _ \n| |\n|_|\n";
+            return self::NUMBER_0;
         }
 
-        return "   \n  |\n  |\n";
+        return self::NUMBER_1;
     }
 }
