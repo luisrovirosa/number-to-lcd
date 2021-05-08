3f4263feeff44a8da593886bcfc00bc9f4cd2c7f
Luis Rovirosa Mairlot <luis@codium.team>
2021-05-08 13:33:17 +0200

    [refactor] inline explaining constants

--- src/NumberToLcd.php
+++ src/NumberToLcd.php
@@ -4,27 +4,17 @@ namespace Kata;
 
 class NumberToLcd
 {
-    private const NUMBER_0 = [" _ ", "| |", "|_|"];
-    private const NUMBER_1 = ["   ", "  |", "  |"];
-    private const NUMBER_2 = [" _ ", " _|", "|_ "];
-    private const NUMBER_3 = [" _ ", " _|", " _|"];
-    private const NUMBER_4 = ["   ", "|_|", "  |"];
-    private const NUMBER_5 = [" _ ", "|_ ", " _|"];
-    private const NUMBER_6 = [" _ ", "|_ ", "|_|"];
-    private const NUMBER_7 = [" _ ", "  |", "  |"];
-    private const NUMBER_8 = [" _ ", "|_|", "|_|"];
-    private const NUMBER_9 = [" _ ", "|_|", " _|"];
     private const NUMBERS = [
-        0 => self::NUMBER_0,
-        1 => self::NUMBER_1,
-        2 => self::NUMBER_2,
-        3 => self::NUMBER_3,
-        4 => self::NUMBER_4,
-        5 => self::NUMBER_5,
-        6 => self::NUMBER_6,
-        7 => self::NUMBER_7,
-        8 => self::NUMBER_8,
-        9 => self::NUMBER_9,
+        0 => [" _ ", "| |", "|_|"],
+        1 => ["   ", "  |", "  |"],
+        2 => [" _ ", " _|", "|_ "],
+        3 => [" _ ", " _|", " _|"],
+        4 => ["   ", "|_|", "  |"],
+        5 => [" _ ", "|_ ", " _|"],
+        6 => [" _ ", "|_ ", "|_|"],
+        7 => [" _ ", "  |", "  |"],
+        8 => [" _ ", "|_|", "|_|"],
+        9 => [" _ ", "|_|", " _|"],
     ];
 
     public function convert(int $decimal): string
